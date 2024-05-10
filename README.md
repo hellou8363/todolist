3b5d6b6c43a4501a9183c0faaae4b730c6de38a4
eunhye han <hellou8363@gmail.com>
2024-05-10 17:30:31 +0900

    🎉 프로젝트 시작

---
+++ README.md
@@ -0,0 +1,30 @@
+## 목차
++ [프로젝트 소개](#프로젝트-소개)
++ [프로젝트 기간](#프로젝트-기간)
++ [개발 도구 및 환경](#개발-도구-및-환경)
++ [Use Case Diagram](#use-case-diagram)
++ [ERD](#erd)
++ [API 명세서](#api-명세서)
++ [결과 화면](#결과-화면)
+
+## 프로젝트 소개
+Todo를 등록, 수정, 삭제할 수 있는 서비스
+
+## 프로젝트 기간
+2024.05.10 ~ (진행중)
+
+## 개발 도구 및 환경
+<img src="https://img.shields.io/badge/kotlin-%237F52FF.svg?style=for-the-badge&logo=kotlin&logoColor=white"/> <img src="https://img.shields.io/badge/springboot-6DB33F?style=for-the-badge&logo=springboot&logoColor=white"> <img src="https://camo.githubusercontent.com/3c0e585bf7fbcca3f142c9c5de6bf415189bfebf5ebe71f59d2efd6272fd8d10/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f537072696e672044617461204a70612d3644423333463f7374796c653d666f722d7468652d6261646765266c6f676f3d737072696e67266c6f676f436f6c6f723d7768697465"> 
+<img src="https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white"> <img src="https://img.shields.io/badge/IntelliJIDEA-000000.svg?style=for-the-badge&logo=intellij-idea&logoColor=white"/> <img src="https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white"/>
+
+## Use Case Diagram
+![USECASEDIAGRAM](https://github.com/hellou8363/todolist/assets/89592727/f4cbf471-fc3f-43b6-8167-4c3cf8e27b04)
+
+## ERD
+![ERD](https://github.com/hellou8363/todolist/assets/89592727/fbdaf0d7-6934-4f1b-ab3e-ccc4d4f2215b)
+
+## API 명세서
+![RESTAPI](https://github.com/hellou8363/todolist/assets/89592727/ca2e4b5b-f48d-4e2a-b935-4d22cd4fa4ca)
+
+## 결과 화면
+(준비중...)
---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
+    kotlin("plugin.noarg") version "1.8.22"
+}
+
+group = "org.zerock"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
