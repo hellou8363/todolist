e023813693ac935e76d79331a3dd0c40e7fbd46a
eunhye han <hellou8363@gmail.com>
2024-05-11 23:59:45 +0900

    ✨ todo repository 추가

---
+++ src/main/kotlin/org/zerock/todolist/domain/todo/repository/TodoRepository.kt
@@ -0,0 +1,7 @@
+package org.zerock.todolist.domain.todo.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.zerock.todolist.domain.todo.model.Todo
+
+interface TodoRepository : JpaRepository<Todo, Long> {
+}
