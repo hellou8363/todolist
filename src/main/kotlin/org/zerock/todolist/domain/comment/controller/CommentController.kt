80bf1109edcaeb7257bdcd99644429be7a1611a0
eunhye han <hellou8363@gmail.com>
2024-05-14 07:40:53 +0900

    ✨ comment controller 추가

---
+++ src/main/kotlin/org/zerock/todolist/domain/comment/controller/CommentController.kt
@@ -0,0 +1,46 @@
+package org.zerock.todolist.domain.comment.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.zerock.todolist.domain.comment.dto.CommentResponse
+import org.zerock.todolist.domain.comment.dto.CreateCommentRequest
+import org.zerock.todolist.domain.comment.dto.DeleteCommentRequest
+import org.zerock.todolist.domain.comment.dto.UpdateCommentRequest
+import org.zerock.todolist.domain.comment.service.CommentService
+
+@RestController
+@RequestMapping("/todos")
+class CommentController(
+    private val commentService: CommentService
+) {
+
+    @PostMapping("/{todoId}/comments")
+    fun createComment(
+        @PathVariable todoId: Long,
+        @RequestBody createCommentRequest: CreateCommentRequest
+    ): ResponseEntity<CommentResponse> {
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(commentService.createComment(todoId, createCommentRequest))
+    }
+
+    @PutMapping("/{todoId}/comments/{commentId}")
+    fun updateComment(
+        @PathVariable todoId: Long,
+        @PathVariable commentId: Long,
+        @RequestBody updateCommentRequest: UpdateCommentRequest
+    ): ResponseEntity<CommentResponse> {
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(commentService.updateComment(todoId, commentId, updateCommentRequest))
+    }
+
+    @DeleteMapping("/{todoId}/comments/{commentId}")
+    fun deleteComment(
+        @PathVariable todoId: Long,
+        @PathVariable commentId: Long,
+        @RequestBody deleteCommentRequest: DeleteCommentRequest
+    ): ResponseEntity<Unit> {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+            .body(commentService.deleteComment(todoId, commentId, deleteCommentRequest))
+    }
+}
