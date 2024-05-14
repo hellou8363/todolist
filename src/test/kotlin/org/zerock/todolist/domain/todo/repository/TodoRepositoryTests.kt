9a3c50201a1d07bf461f33e91b64b09472c78358
eunhye han <hellou8363@gmail.com>
2024-05-14 09:14:35 +0900

    ♻️ todo reponsitory 테스트 코드 수정

--- src/test/kotlin/org/zerock/todolist/domain/todo/repository/TodoRepositoryTests.kt
+++ src/test/kotlin/org/zerock/todolist/domain/todo/repository/TodoRepositoryTests.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.repository.findByIdOrNull
 import org.zerock.todolist.domain.exception.ModelNotFoundException
 import org.zerock.todolist.domain.todo.model.Todo
+import org.zerock.todolist.domain.todo.model.TodoCompleted
 import org.zerock.todolist.domain.todo.model.toResponse
 
 @SpringBootTest
@@ -16,9 +17,9 @@ class TodoRepositoryTests {
     private lateinit var todoRepository: TodoRepository
 
     @Test
-    @DisplayName("100개의 데이터 삽입")
+    @DisplayName("10개의 데이터 삽입")
     fun insertTest() {
-        for (i in 1..100) {
+        for (i in 1..10) {
             val todo = Todo(title = "title...$i", content = "content...$i", writer = "writer...$i")
             todoRepository.save(todo)
         }
@@ -33,7 +34,7 @@ class TodoRepositoryTests {
     @Test
     @DisplayName("단 건 Todo 조회")
     fun selectByIdTest() {
-        val id = 100L
+        val id = 123L
         val todo = todoRepository.findByIdOrNull(id) ?: throw ModelNotFoundException("Todo", id)
         println(todo.toResponse())
     }
@@ -41,7 +42,7 @@ class TodoRepositoryTests {
     @Test
     @DisplayName("Todo 삭제")
     fun deleteTest() {
-        val id = 100L
+        val id = 124L
         val todo = todoRepository.findByIdOrNull(id) ?: throw ModelNotFoundException("Todo", id)
         todoRepository.delete(todo)
     }
@@ -49,12 +50,13 @@ class TodoRepositoryTests {
     @Test
     @DisplayName("Todo 수정")
     fun updateTest() {
-        val id = 101L
+        val id = 123L
         val todo = todoRepository.findByIdOrNull(id) ?: throw ModelNotFoundException("Todo", id)
 
-        todo.title = "Todo 수정 테스트"
-        todo.content = "Todo 수정 테스트"
-        todo.writer = "Todo 수정 테스트"
+//        todo.title = "Todo 수정 테스트"
+//        todo.content = "Todo 수정 테스트"
+//        todo.writer = "Todo 수정 테스트"
+        todo.completed = TodoCompleted.TRUE
 
         val result = todoRepository.save(todo)
         println(result.toResponse())
