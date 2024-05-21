b3cbcd97126bd7ed4159ddba88d537d6c9511166
eunhye han <hellou8363@gmail.com>
2024-05-21 19:23:20 +0900

    ✨ refresh token 발급 기능 추가

---
+++ src/main/kotlin/org/zerock/todolist/util/ApiRefreshController.kt
@@ -0,0 +1,60 @@
+package org.zerock.todolist.util
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+@RestController
+class ApiRefreshController(
+    private val jwtUtil: JwtUtil
+) {
+
+    @PostMapping("/api/user/refresh")
+    fun refresh(
+        @RequestHeader("Authorization") authHeader: String,
+        @RequestBody refreshToken: String
+    ): Map<String, Any> {
+        if (refreshToken == null) {
+            throw CustomJwtException("NULL_REFRESH")
+        }
+
+        if (authHeader == null || authHeader.length < 7) {
+            throw CustomJwtException("INVALID_STRING")
+        }
+
+        val accessToken = authHeader.substring(7)
+
+        if (!checkExpiredToken(accessToken)) { // AccessToken이 만료되지 않았다면 그대로 반환
+            return mapOf("accessToken" to accessToken, "refreshToken" to refreshToken)
+        }
+
+        val claims = jwtUtil.validateToken(refreshToken)
+        val newAccessToken = jwtUtil.generateToken(claims, 10) // 10분
+        val newRefreshToken =
+            if (checkTime(claims["exp"] as Int)) jwtUtil.generateToken(claims, 60 * 24) else refreshToken
+
+        return mapOf("accessToken" to newAccessToken, "refreshToken" to newRefreshToken)
+    }
+
+    fun checkTime(exp: Int): Boolean { // 1시간 미만 여부
+        val expDate = Date(exp.toLong() * 1000) // JWT exp를 날짜로 변환
+        val gap = expDate.time - System.currentTimeMillis() // 현재 시간과 차이 계산(ms)
+        val leftMin = gap / (1000 * 60)
+        println("checkTime - expDate: $expDate, gap: $gap, leftMin: $leftMin")
+
+        return leftMin < exp
+    }
+
+    fun checkExpiredToken(token: String): Boolean {
+        try {
+            jwtUtil.validateToken(token)
+        } catch (e: CustomJwtException) {
+            if (e.message == "Expired") {
+                return true
+            }
+        }
+        return false
+    }
+}
