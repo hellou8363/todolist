fe54e0e26a24e8d1719f0a8ff4b2f7ffd1b9dd0b
eunhye han <hellou8363@gmail.com>
2024-05-22 19:14:23 +0900

    ✨ swagger에 access token 입력을 위한 코드 추가

--- src/main/kotlin/org/zerock/todolist/infra/swagger/SwaggerConfig.kt
+++ src/main/kotlin/org/zerock/todolist/infra/swagger/SwaggerConfig.kt
@@ -3,6 +3,8 @@ package org.zerock.todolist.infra.swagger
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -10,6 +12,15 @@ import org.springframework.context.annotation.Configuration
 class SwaggerConfig {
 
     @Bean
-    fun openApi() = OpenAPI().components(Components())
-        .info(Info().title("Todo API").description("Todo API schema").version("1.0.0"))
+    fun openApi(): OpenAPI? {
+        val apiKey = SecurityScheme()
+            .type(SecurityScheme.Type.APIKEY)
+            .`in`(SecurityScheme.In.HEADER)
+            .name("Authorization")
+
+        val securityRequirement = SecurityRequirement().addList("Bearer Token")
+        return OpenAPI().components(Components().addSecuritySchemes("Bearer Token", apiKey))
+            .addSecurityItem(securityRequirement)
+            .info(Info().title("Todo API").description("Todo API schema").version("1.0.0"))
+    }
 }
