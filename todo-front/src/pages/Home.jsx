d6472bbe67d52da74af423d03652c41c33f87c78
eunhye han <hellou8363@gmail.com>
2024-05-25 23:42:43 +0900

    ➕ ✨ todo 목록 조회 페이징 처리 및 페이징 처리를 위한 의존성 추가

--- todo-front/src/pages/Home.jsx
+++ todo-front/src/pages/Home.jsx
@@ -1,16 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import Card from "../components/Card";
 import "./Home.css";
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
+  const [page, setPage] = useState(0);
+  const totalCount = useRef(1);
+  const [ref, inView] = useInView();
 
   useEffect(() => {
-    fetch("http://localhost:8080/todos")
+    if (inView && todos.length < totalCount.current) {
+      console.log("todos.length: ", todos.length, ", totalCount: ", totalCount);
+      todoFetch();
+    }
+  }, [inView]);
+
+  const todoFetch = () => {
+    fetch(`http://localhost:8080/todos?page=${page}&size=10`)
       .then((res) => res.json())
-      .then((data) => setTodos([...todos, ...data.content]))
-      .catch((err) => console.log(err.message));
-  }, []);
+      .then((data) => {
+        setTodos([...todos, ...data.content]);
+        setPage((page) => page + 1);
+        totalCount.current = data.totalElements;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <>
@@ -26,6 +43,7 @@ const Home = () => {
             />
           );
         })}
+        <div ref={ref}></div>
       </div>
     </>
   );
