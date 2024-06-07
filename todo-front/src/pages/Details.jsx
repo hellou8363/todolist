9e2ecd51899c68546b659bbb49eeb709aa7907de
eunhye han <hellou8363@gmail.com>
2024-06-07 23:12:00 +0900

    ✨ Todo 상세 페이지 추가 - 수정, 삭제 버튼 및 할일 완료 버튼 추가 필요 - CSS 추가 필요 - 서버에 단건에 대한 요청을 보내지 않고 Todo 목록의 정보를 상세 페이지로 전달(서버에 요청하는 횟수를 줄일 수 있다는 장점)

--- todo-front/src/pages/Details.jsx
+++ todo-front/src/pages/Details.jsx
@@ -1,5 +1,26 @@
+import { useLocation } from "react-router-dom";
+import "./Details.css";
+
 const Details = () => {
-  return <>Details Page</>;
+  const location = useLocation();
+  const todo = { ...location.state };
+  const date = new Date(todo.createdAt);
+  const createDate = `${date.getFullYear()}-${
+    date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth()
+  }-${date.getDay() < 10 ? "0" + date.getDay() : date.getDay()}`;
+
+  return (
+    <>
+      <div className="details-card">
+        <div className="info_wrap">
+          <p className="title">{todo.title}</p>
+          <p className="writer">{todo.writer}</p>
+        </div>
+        <p className="date">{createDate}</p>
+        <p className="content">{todo.content}</p>
+      </div>
+    </>
+  );
 };
 
-export default Details
+export default Details;
