f1c1b69c8b749d285683df906427e9bf6e503589
eunhye han <hellou8363@gmail.com>
2024-06-19 17:07:26 +0900

    ♻️ Todo 수정 시 개별 값 수정이 가능하도록 수정

--- todo-backend/src/main/kotlin/org/zerock/todolist/domain/todo/dto/UpdateTodoRequest.kt
+++ todo-backend/src/main/kotlin/org/zerock/todolist/domain/todo/dto/UpdateTodoRequest.kt
@@ -1,12 +1,13 @@
 package org.zerock.todolist.domain.todo.dto
 
 import jakarta.validation.constraints.Size
+import org.zerock.todolist.domain.todo.model.TodoCompleted
 
 data class UpdateTodoRequest(
     @field:Size(min = 1, max = 200, message = "Title must be between 1 and 200")
-    val title: String,
+    val title: String?,
     @field:Size(min = 1, max = 1000, message = "Content must be between 1 and 1000")
-    val content: String,
-    val writer: String,
-    val completed: Boolean,
+    val content: String?,
+    val writer: String?,
+    val completed: TodoCompleted?,
 )
