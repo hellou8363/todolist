ba9a4af9e67f80c06b8ec4f62d76e114901de7d1
eunhye han <hellou8363@gmail.com>
2024-06-19 17:09:11 +0900

    ♻️ 유저 정보를 가져올 때 email이 아닌 userId로 가져오도록 수정

--- todo-backend/src/main/kotlin/org/zerock/todolist/config/auth/UserDetailServiceImpl.kt
+++ todo-backend/src/main/kotlin/org/zerock/todolist/config/auth/UserDetailServiceImpl.kt
@@ -1,11 +1,10 @@
 package org.zerock.todolist.config.auth
 
-import org.springframework.security.authentication.InternalAuthenticationServiceException
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
-import org.zerock.todolist.domain.user.model.User
 import org.zerock.todolist.domain.user.repository.UserRepository
 
 
@@ -14,18 +13,11 @@ class UserDetailServiceImpl(
     private val userRepository: UserRepository
 ) : UserDetailsService {
 
-    override fun loadUserByUsername(email: String): UserDetails {
-        var user: User
+    override fun loadUserByUsername(userId: String): UserDetails {
+        val user = userRepository.findByIdOrNull(userId.toLong()) ?: throw UsernameNotFoundException(userId)
 
-        try {
-            user = userRepository.findByEmail(email)
-
-            if(!user.joinType.contains("EMAIL")) {
-                throw UsernameNotFoundException(email)
-            }
-
-        } catch (e: InternalAuthenticationServiceException) {
-            throw UsernameNotFoundException(email)
+        if (!user.joinType.contains("EMAIL")) {
+            throw UsernameNotFoundException(userId)
         }
 
         return CustomUserDetails(user)
