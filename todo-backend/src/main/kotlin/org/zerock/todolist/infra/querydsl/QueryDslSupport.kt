850659cd45b85e4159a69d9bb07131ee593c376c
eunhye han <hellou8363@gmail.com>
2024-06-21 10:42:08 +0900

    ♻️ JPAQueryFactory를 Bean으로 등록

--- todo-backend/src/main/kotlin/org/zerock/todolist/infra/querydsl/QueryDslSupport.kt
+++ todo-backend/src/main/kotlin/org/zerock/todolist/infra/querydsl/QueryDslSupport.kt
@@ -1,14 +1,14 @@
-package org.zerock.todolist.infra.querydsl
-
-import com.querydsl.jpa.impl.JPAQueryFactory
-import jakarta.persistence.EntityManager
-import jakarta.persistence.PersistenceContext
-
-abstract class QueryDslSupport { // 직접 사용하지 않으므로 추상 클래스로 만듦
-
-    @PersistenceContext
-    protected lateinit var entityManager: EntityManager
-
-    protected val queryFactory: JPAQueryFactory
-        get() = JPAQueryFactory(entityManager)
-}
+//package org.zerock.todolist.infra.querydsl
+//
+//import com.querydsl.jpa.impl.JPAQueryFactory
+//import jakarta.persistence.EntityManager
+//import jakarta.persistence.PersistenceContext
+//
+//abstract class QueryDslSupport { // 직접 사용하지 않으므로 추상 클래스로 만듦
+//
+//    @PersistenceContext
+//    protected lateinit var entityManager: EntityManager
+//
+//    protected val queryFactory: JPAQueryFactory
+//        get() = JPAQueryFactory(entityManager)
+//}
