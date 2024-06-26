9e7408daa3dbae8470d3f5ae4ce72d8179f85089
eunhye han <hellou8363@gmail.com>
2024-06-26 09:00:00 +0900

    ➕ @MockBean 어노테이션 사용과 S3 이미지 업로드를 위한 의존성 추가

--- todo-backend/build.gradle.kts
+++ todo-backend/build.gradle.kts
@@ -72,9 +72,12 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
 
     implementation("org.springframework.boot:spring-boot-starter-aop")
     runtimeOnly("com.h2database:h2")
+
+    implementation("io.awspring.cloud:spring-cloud-aws-starter-s3:3.1.1")
 }
 
 tasks.withType<KotlinCompile> {
